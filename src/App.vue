d0d3359ee6583c3463a99c0fd7014bc32fe35974
CalDey <2234706312@qq.com>
2021-02-27 12:13:38 +0800

    封装Storage

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-
+import storage from './storage'
 export default {
   name: 'App',
   data() {
@@ -17,7 +17,10 @@ export default {
 
   },
   mounted() {
-
+    // storage.setItem('user', {a:1})
+    // storage.setItem('abc', {a:1}, 'user')
+    // storage.clear('a')
+    storage.clear('a', 'user')
   }
 }
 </script>
